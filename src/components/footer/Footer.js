5ec3b3abc8d593c5c227f369200f5b1565b1b861
onam <onamkrverma@gmail.com>
2024-01-20 13:29:29 +0530

    update footer text

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,15 +1,19 @@
-import React from 'react'
-import './Footer.css'
+import React from "react";
+import "./Footer.css";
 
 const Footer = () => {
   return (
-    <div className='footer-section'>
-      <p>This site created for educational purposes only!!</p>
-      <p> &copy; 2023 created by ✌
-        <a href="https://onam.netlify.app/"  rel="noreferrer" target="_blank" > onamkrverma</a>
-        </p>
+    <div className="footer-section">
+      <p>This web app is made only for educational purposes!!</p>
+      <p>
+        &copy; 2023 created by ✌
+        <a href="https://onam.netlify.app/" rel="noreferrer" target="_blank">
+          {" "}
+          onamkrverma
+        </a>
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
