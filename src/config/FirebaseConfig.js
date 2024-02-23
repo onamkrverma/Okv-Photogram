4263956b6be9e54fd117813c1dea93d23189ad26
onam <onamkrverma@gmail.com>
2024-02-23 23:05:21 +0530

    fix: CJS build error of Vite

--- src/config/FirebaseConfig.js
+++ src/config/FirebaseConfig.js
@@ -1,21 +1,20 @@
-import {initializeApp} from 'firebase/app'
-import {getAuth} from 'firebase/auth'
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC9NnFzOvfmk7gzJpfsnTjq2AjeOw01O5I",
+  apiKey: import.meta.env.VITE_API_KEY,
   authDomain: "instagram-clone-a8b82.firebaseapp.com",
   projectId: "instagram-clone-a8b82",
   storageBucket: "instagram-clone-a8b82.appspot.com",
   messagingSenderId: "705527732931",
   appId: "1:705527732931:web:073a12ebb8053c41a1a076",
-  measurementId: "G-SXP4CMXNE2"
+  measurementId: "G-SXP4CMXNE2",
 };
 
-
-const app = initializeApp(firebaseConfig)
+const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const db = getFirestore(app)
-const storage = getStorage(app)
-export  {app,auth,db,storage};
+const db = getFirestore(app);
+const storage = getStorage(app);
+export { app, auth, db, storage };
